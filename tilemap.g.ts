3049f0ddb8b6f6e6a60cfaf30d1c5d55eca3d688
Ivan Wang <ivawzh@gmail.com>
2021-06-14 00:04:33 +1000

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,5 +1,23 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Green Obstacle":
+            case "tile2":return tile2;
+            case "Red Obstacle":
+            case "tile1":return tile1;
+            case "Blue Obstacle":
+            case "tile3":return tile3;
+        }
+        return null;
+    })
 
 }
 // Auto-generated code. Do not edit.
