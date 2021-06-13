3b7fcc37cc5533ae3d06b352b0eca8a8bb77a851
Ivan Wang <ivawzh@gmail.com>
2021-06-13 23:10:25 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,69 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "ah red":return img`
+. . 5 5 5 . . . . 5 5 5 . . . . 
+. 5 2 2 2 e . . e 2 2 2 5 . . . 
+5 2 2 2 2 2 e e 2 2 2 2 2 5 . . 
+5 2 2 5 5 2 2 2 2 5 5 2 2 5 . . 
+e 2 5 5 2 2 2 2 2 2 5 5 2 e . . 
+. e e 2 2 2 2 2 2 2 2 e e . . . 
+. . e 2 f 2 2 2 2 f 2 e . . . . 
+. . f 2 2 2 5 5 2 2 2 f . . f f 
+. . f 5 2 2 f f 2 2 6 f . f 2 f 
+. . . f 6 6 6 6 6 6 5 5 f 2 2 f 
+. . . f 5 2 2 2 2 2 2 5 5 2 f . 
+. . . f 2 2 2 2 2 5 2 2 f f . . 
+. . . f 2 f f f 2 f f 2 f . . . 
+. . . f f . . f f . . f f . . . 
+`;
+            case "image3":
+            case "ah blue":return img`
+. . 8 8 8 . . . . 8 8 8 . . . . 
+. 8 6 6 6 e . . e 6 6 6 8 . . . 
+8 6 6 6 6 6 e e 6 6 6 6 6 8 . . 
+8 6 6 8 8 6 6 6 6 8 8 6 6 8 . . 
+e 6 8 8 6 6 6 6 6 6 8 8 6 e . . 
+. e e 6 6 6 6 6 6 6 6 e e . . . 
+. . e 6 f 6 6 6 6 f 6 e . . . . 
+. . f 6 6 6 8 8 6 6 6 f . . f f 
+. . f 8 6 6 f f 6 6 4 f . f 6 f 
+. . . f 4 4 4 4 4 4 8 8 f 6 6 f 
+. . . f 8 6 6 6 6 6 6 8 8 6 f . 
+. . . f 6 6 6 6 6 8 6 6 f f . . 
+. . . f 6 f f f 6 f f 6 f . . . 
+. . . f f . . f f . . f f . . . 
+`;
+            case "image2":
+            case "ah green":return img`
+. . c c c . . . . c c c . . . . 
+. c 7 7 7 e . . e 7 7 7 c . . . 
+c 7 7 7 7 7 e e 7 7 7 7 7 c . . 
+c 7 7 c c 7 7 7 7 c c 7 7 c . . 
+e 7 c c 7 7 7 7 7 7 c c 7 e . . 
+. e e 7 7 7 7 7 7 7 7 e e . . . 
+. . e 7 f 7 7 7 7 f 7 e . . . . 
+. . f 7 7 7 c c 7 7 7 f . . f f 
+. . f c 7 7 f f 7 7 3 f . f 7 f 
+. . . f 3 3 3 3 3 3 c c f 7 7 f 
+. . . f c 7 7 7 7 7 7 c c 7 f . 
+. . . f 7 7 7 7 7 c 7 7 f f . . 
+. . . f 7 f f f 7 f f 7 f . . . 
+. . . f f . . f f . . f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,5 @@
+let mySprite = sprites.create(assets.image`ah red`, SpriteKind.Player)
+const screenCenter = scene.screenWidth() /2 
+mySprite.y = scene.screenHeight() / 3 * 2
+mySprite.x = scene.screenWidth()
+
